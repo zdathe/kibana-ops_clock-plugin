872bf604984f39e44880291947576a991ab3b868
zdathe <zdathe@yahoo.com>
2017-06-07 19:47:30 -0500

    Create index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function (kibana) {
   return new kibana.Plugin({
 
     uiExports: {
-      visTypes: ['plugins/kibana-ops_clock-plugin/radial_clockâ€™]
+      visTypes: ['plugins/kibana-ops_clock-plugin/radial_clock']
     }
 
   });
